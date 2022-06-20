77038cf3e6332b9177ad77b0b30cae08e63ad31f
bruce oyugi <oyugibruce2017@gmail.com>
2022-06-20 16:47:28 +0300

    0x05. Python - Exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
+
+
 def safe_print_list(my_list=[], x=0):
-    index = 0
-    while True:
-        try:
-            if index < x:
-                print(my_list[index], end='')
-                index += 1
-            else:
-                print()
-                return index
-        except IndexError:
-            print()
-            return 
+    i = 0
+    try:
+        while i is not x:
+            print(my_list[i], end='')
+            i += 1
+    except IndexError:
+        None
+    print()
+    return i
