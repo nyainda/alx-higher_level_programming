64aa36471880af740fa212ed0189a31fd8e2ce20
bruce oyugi <oyugibruce2017@gmail.com>
2022-06-20 17:01:51 +0300

    0x05. Python - Exceptions

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    result = 0
-    return result
+    magic = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                magic += a ** b / i
+        except:
+            magic = b + a
+            break
+    return magic
