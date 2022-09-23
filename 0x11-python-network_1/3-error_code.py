a74c5bdde8130737bb073182d28f13c71275adf3
bruce oyugi <oyugibruce2017@gmail.com>
2022-09-23 23:30:58 +0300

    0x11. Python - Network #1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""
-Takes in URL, sends request to it
-and displays body of response decoded in utf-8.
+""" 
+Script that takes in a URL, send a request to URL, and dispaly body
 """
 
-import urllib.request as request
-import urllib.parse as parse
-from urllib import error
-from sys import argv
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    import sys
+    from urllib import request, error
+
     try:
-        with request.urlopen(argv[1]) as r:
-            print(r.read().decode('utf-8'))
-    except error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+        with request.urlopen(sys.argv[1]) as resp:
+            print(resp.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-""" Uses requests module to fetch URL status"""
-
+"""
+Script that fetches https://intranet.hbtn.io/status
+"""
 import requests
 
 if __name__ == '__main__':
-    response = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:\n\t- type: {}\n\t- content: {}"
-          .format(type(response.text), response.text))
+    url = "https://intranet.hbtn.io/status"
+    r = requests.get(url)
+    text = r.text
+    print("Body response:")
+    print("\t- type: {}".format(type(text)))
+    print("\t- content: {}".format(text))
